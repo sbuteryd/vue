f081f63601c26096689898a34f2877811b2d9d3f
sbuteryd@qq.com <sbuteryd@qq.com>
2019-10-08 20:15:11 +0800

    lesson3

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var app = new Vue ({
     data:{
         product:"Socks",
         image:'./assets/vmSocks-green-onWhite.jpg',
-        altText:"a pair of socks"
+        inStock:false,
+        inventory:100
     }
 })

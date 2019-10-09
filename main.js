df45f1d25c1b98ec95e68d22612c362b5444cf30
sbuteryd@qq.com <sbuteryd@qq.com>
2019-10-10 07:02:15 +0800

    4 list rendering

--- main.js
+++ main.js
@@ -3,7 +3,18 @@ var app = new Vue({
     data: {
         product: 'Socks',
         image: './assets/vmSocks-green-onWhite.jpg',
-        inStock: true
+        inStock: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ]
     }
 })
 

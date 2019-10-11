6314cd702955deae2f109643ec9f9313254570e3
sbuteryd@qq.com <sbuteryd@qq.com>
2019-10-11 21:10:42 +0800

    lesson 5

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
 var app  = new Vue({
     el:'#app',
     data:{
-        product:"Socks"
+        product:"Socks",
+        description:'this is socks',
+        inStock:true,
+        image:'./img/vmSocks-green-onWhite.jpg',
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                image:'./img/vmSocks-green-onWhite.jpg',
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+                image:'./img/vmSocks-blue-onWhite.jpg',
+            }
+        ],
+        cart:0,
+    },
+    methods:{
+        addCarts(){
+            this.cart +=1
+        },
+        updateProduct(image){
+            this.image = image
+        }
     }
 })

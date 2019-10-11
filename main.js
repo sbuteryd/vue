0331da150edb260f27d71395e51dcb8b9140b419
sbuteryd@qq.com <sbuteryd@qq.com>
2019-10-12 07:24:41 +0800

    practice 1

--- main.js
+++ main.js
@@ -10,38 +10,36 @@ var app  = new Vue({
             {
                 variantId: 2234,
                 variantColor: 'green',
-                image:'./img/vmSocks-green-onWhite.jpg',
+                variantimage:'./img/vmSocks-green-onWhite.jpg',
                 variantQuantity:10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
-                image:'./img/vmSocks-blue-onWhite.jpg',
+                variantimage:'./img/vmSocks-blue-onWhite.jpg',
                 variantQuantity:0
-
             }
         ],
-        cart:0,
+        carts:0,
     },
     methods:{
-        addCarts(){
-            this.cart +=1
-        },
-        updateProduct(index){
+        updateImage(index){
             this.selectedVariant = index
-            console.log(index)
+        },
+        updateCarts(){
+            this.carts +=1
         }
     },
-    computed :{
-        title(){
-            return this.brand + " " +this.product
-        },
+    computed:{
         image(){
-            return this.variants[this.selectedVariant].image
+            return this.variants[this.selectedVariant].variantimage
+        },
+        title(){
+            return `${this.brand} ${this.product}`
         },
         inStock(){
             return this.variants[this.selectedVariant].variantQuantity
         }
-    },
+    }
 
 })

450f94055e650b020ce76cd0133e39035bcad819
sbuteryd@qq.com <sbuteryd@qq.com>
2019-10-12 06:41:41 +0800

    lesson 7

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 var app  = new Vue({
     el:'#app',
     data:{
+        brand:"Vue Mastery",
         product:"Socks",
         description:'this is socks',
-        inStock:true,
-        image:'./img/vmSocks-green-onWhite.jpg',
+        selectedVariant:0,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 image:'./img/vmSocks-green-onWhite.jpg',
+                variantQuantity:10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 image:'./img/vmSocks-blue-onWhite.jpg',
+                variantQuantity:0
+
             }
         ],
         cart:0,
@@ -24,8 +27,21 @@ var app  = new Vue({
         addCarts(){
             this.cart +=1
         },
-        updateProduct(image){
-            this.image = image
+        updateProduct(index){
+            this.selectedVariant = index
+            console.log(index)
         }
-    }
+    },
+    computed :{
+        title(){
+            return this.brand + " " +this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].image
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        }
+    },
+
 })

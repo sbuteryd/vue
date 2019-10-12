0c537d315bb260c688c5d4cb707b3f2b8a0888b6
sbuteryd@qq.com <sbuteryd@qq.com>
2019-10-12 20:47:06 +0800

    pratice 3

--- main.js
+++ main.js
@@ -22,13 +22,24 @@ var app  = new Vue({
         ],
         carts:0,
     },
+    methods:{
+        updateImage(index){
+            this.selectedVariant = index
+        },
+        updateCart(){
+            this.carts +=1
+        }
+    },
     computed:{
         image(){
             return this.variants[this.selectedVariant].variantimage
         },
         title(){
-            return  `${this.brand} ${this.product}`
+            return `${this.brand} ${this.product}`
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
         }
-    }
+    },
 
 })
